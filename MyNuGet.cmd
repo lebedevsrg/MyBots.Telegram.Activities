fb4316b3fb4e3ae44c80da80404a2df91928f981
Sergey Lebedev <lebedev.srg@ya.ru>
2019-12-09 23:02:24 +0300

    Second Commit

---
+++ MyNuGet.cmd
@@ -0,0 +1,12 @@
+
+IF EXIST "$(ProjectDir)$(OutDir)*.nupkg" del "$(ProjectDir)$(OutDir)*.nupkg"
+nuget.exe pack "$(ProjectPath)"
+IF EXIST "$(ProjectDir)$(OutDir)*.nupkg" xcopy /Y "$(ProjectDir)$(OutDir)*.nupkg" "C:\ProgramData\UiPath\Packages\"
+
+cd /D "D:\DevOps\UIPath\UIPath.Telegram.Activities"
+
+nuget pack  -NoDefaultExcludes
+
+nuget locals -list global-packages
+
+nuget.exe pack "MyBots.Telegram.Activities.csproj" -NoDefaultExcludes
